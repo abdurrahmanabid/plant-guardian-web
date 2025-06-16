7c44e8f29821c9049233a3a0b9ba32974fcf5aad
abdurrahmanabid <abdurrahmanabid33@gmail.com>
2025-06-17 01:02:58 +0600

    something new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import { getFirstName, getLastName, getName } from "../helper/getNmae";
+import LOGO from "../assets/img/logo-no-bg.png";
 
 const Navbar = () => {
   return (
     <div>
       {/* Navbar */}
       <nav className="relative z-50 text-white dark:text-red-200 dark:bg-emerald-700">
-        <div className="container mx-auto px-4 py-3 flex justify-center md:justify-between items-center">
+        <div className="container mx-auto px-4 py-1 flex justify-center md:justify-between items-center">
           {/* Logo */}
+          <img src={LOGO} alt={getName} className="h-20" />
           <div className="text-2xl font-bold font-exo">
             {getFirstName()}
             <span className="text-yellow-300 dark:text-yellow-500">
