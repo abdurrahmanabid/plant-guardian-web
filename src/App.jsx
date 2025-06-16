3e958e4a0b6e8bd3a5ee6562be450bcb9ecf5920
abdurrahmanabid <abdurrahmanabid33@gmail.com>
2025-06-17 00:15:18 +0600

    tailwind router-dom something are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
-const App = () => {
-  return (
-    <div className=''>This is the App</div>
-  )
-}
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Home from "./pages/Home";
 
-export default App
+const App = () => {
+  const AppLayout = () => {
+    return (
+      <>
+        <div className="">Nabbar</div>
+        <Outlet />
+      </>
+    );
+  };
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
